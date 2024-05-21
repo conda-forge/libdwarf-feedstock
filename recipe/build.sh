ef56cbc1b08108f1bb50e5e619f745bbbba2f1b5
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-05-21 15:04:48 -0500

    Make copies of dwarf.h and libdwarf.so.0 in the old locations

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,3 +2,8 @@
 
 cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=YES -DBUILD_NON_SHARED=NO
 cmake --build build --config Release --target install
+
+# backwards compatibility
+cp -v $PREFIX/lib/libdwarf${SHLIB_EXT} $PREFIX/lib/libdwarf${SHLIB_EXT}.0
+mkdir $PREFIX/include/libdwarf-0
+cp -v $PREFIX/include/dwarf.h $PREFIX/include/libdwarf-0/dwarf.h
