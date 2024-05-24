d84f06c40917e25b4ad3ef059fa66ff18fce3cc4
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-05-23 21:38:56 -0500

    Try to use SOVERSION

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,3 @@
 
 cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=YES -DBUILD_NON_SHARED=NO
 cmake --build build --config Release --target install
-
-# backwards compatibility
-cp -v $PREFIX/lib/libdwarf${SHLIB_EXT} $PREFIX/lib/libdwarf${SHLIB_EXT}.0
